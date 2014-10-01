01846ef8fba4a3e51f1dfed4fd934ab5c486576c
Pc-1 Agencia MIT <phydokz@hotmail.com>
2014-10-01 17:43:32 -0400

    material

--- js/shapes/Regular.js
+++ js/shapes/Regular.js
@@ -8,10 +8,10 @@ function Regular(center, radius, tips, thickness, theta) {
     this.updateVertices = function () {
         var ga = (360 / this.tips);
         var gb = (360 / this.tips) / 2;
-        var pa = [this.center[0], this.center[1] - this.radius * this.thickness];
-        var pb = [this.center[0], this.center[1] - this.radius];
-        pa = MV.rotate(pa, this.theta, this.center);
-        pb = MV.rotate(pb, this.theta + gb, this.center);
+        var pa = [0,- this.radius * this.thickness];
+        var pb = [0,- this.radius];
+        pa = MV.rotate(pa, -this.theta, [0,0]);
+        pb = MV.rotate(pb, -this.theta - gb, [0,0]);
 
         this.vertices.push(pa);
         this.vertices.push(pb);
@@ -19,8 +19,8 @@ function Regular(center, radius, tips, thickness, theta) {
         for (var i = 1; i < this.tips; i++) {
             pa = [pa[0], pa[1]];
             pb = [pb[0], pb[1]];
-            pa = MV.rotate(pa, ga, this.center);
-            pb = MV.rotate(pb, ga, this.center);
+            pa = MV.rotate(pa, -ga, [0,0]);
+            pb = MV.rotate(pb, -ga, [0,0]);
             this.vertices.push(pa);
             this.vertices.push(pb);
         }
