d938d43e1189a8201737be573644f50f903a394b
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-01-18 15:25:20 -0300

    protótipo Rect

--- js/shapes/Rect.js
+++ js/shapes/Rect.js
@@ -14,52 +14,51 @@ Rect.validateHeight = function(height){
 function Rect(center, width, height) {
     var self = this;
     Polygon.call(self, center, 0);
-
     Rect.validateWidth(width);
     Rect.validateHeight(height);
     self.width =  width;
     self.height = height;
     self.parent = null;
 
-    self.setWidth = function (width) {
-        var self = this;
-        Rect.validateWidth(width);
-        self.updateDimen(width, self.height);
-    };
-
-    self.setHeight = function (height) {
-        var self = this;
-        Rect.validateHeight(height);
-        self.updateDimen(self.width, height);
-    };
-
-    self.updateDimen = function (width, height) {
-        var self = this;
-        Rect.validateWidth(width);
-        Rect.validateHeight(height);
-        self.width = width;
-        self.height = height;
-        self.area = self.width * self.height;
-        var mw = self.width * 0.5;
-        var mh = self.height * 0.5;
-        self.vertices = [
-            [mw, -mh],
-            [-mw, -mh],
-            [-mw, mh],
-            [mw, mh]
-        ];
-        self.updateMinAndMax();
-        if (self.parent != null) {
-            self.parent.update();
-        }
-    };
-
-    self.moi = function (mass) {
-        var self = this;
-        return mass / 12 * (self.height * self.height + self.width * self.width);
-    };
-
     self.updateDimen(self.width, self.height);
 }
+
+Rect.prototype.setWidth = function(width){
+    var self = this;
+    Rect.validateWidth(width);
+    self.updateDimen(width, self.height);
+};
+
+Rect.prototype.setHeight = function(height){
+    var self = this;
+    Rect.validateHeight(height);
+    self.updateDimen(self.width, height);
+};
+
+Rect.prototype.moi = function(mass){
+    var self = this;
+    return mass / 12 * (self.height * self.height + self.width * self.width);
+};
+
+Rect.prototype.updateDimen = function(width,height){
+    var self = this;
+    Rect.validateWidth(width);
+    Rect.validateHeight(height);
+    self.width = width;
+    self.height = height;
+    self.area = self.width * self.height;
+    var mw = self.width * 0.5;
+    var mh = self.height * 0.5;
+    self.vertices = [
+        [mw, -mh],
+        [-mw, -mh],
+        [-mw, mh],
+        [mw, mh]
+    ];
+    self.updateMinAndMax();
+    if (self.parent != null) {
+        self.parent.update();
+    }
+};
 
 
