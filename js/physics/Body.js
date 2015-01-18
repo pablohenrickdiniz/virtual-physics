b8b5fb504bf80ae0d088d4f6f10491ffd41eaed3
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-01-18 15:46:27 -0300

    protótipo Body

--- js/physics/Body.js
+++ js/physics/Body.js
@@ -16,114 +16,114 @@ function Body(shape, material, dinamic, vLin, vAng) {
     self.vertsAbsolute = null;
     self.inLeaf = [];
     // undefined is center of mass.
-
-    var rotMatTheta; // used to avoid unnecessary rotation matrix computations
-    var rotationMatrix;
-
-    self.addLeaf = function(quad){
-        var self = this;
-        self.inLeaf.push(quad);
-    };
-
-    self.update = function () {
-        var self = this;
-        self.mass = self.dinamic ? self.material.density * self.shape.area : 0;
-        self.mInv = self.mass == 0 ? 0 : 1 / self.mass; // inverse of the mass
-        self.moiInv = self.mass == 0 ? 0 : 1 / self.shape.moi(self.mass);// inverse of the moment of inertia
-        self.forces = [];
-        if (self.dinamic) {
-            self.forces.push([0, 98.81 * self.mass]);
-        }
-        else {
-            self.vLin = [0, 0];
-            self.vAng = 0;
-        }
-        rotationMatrix = null;
-    };
-
-    self.setDinamic = function (dinamic) {
-        var self = this;
-        self.dinamic = dinamic;
-        self.update();
-    };
-
-    self.setMass = function (mass) {
-        var self = this;
-        self.mass = mass;
-        self.mInv = self.mass == 0 ? 0 : 1 / self.mass; // inverse of the mass
-        self.moiInv = self.mass == 0 ? 0 : 1 / self.shape.moi(self.mass);// inverse of the moment of inertia
-        rotationMatrix = null;
-    };
-
-    self.setShape = function (shape) {
-        var self = this;
-        self.shape = shape;
-        self.shape.parent = self;
-        self.update();
-    };
-
-    self.setMaterial = function (material) {
-        var self = this;
-        self.material = material;
-        self.update();
-    };
-
-    self.setCenter = function(center){
-        Point.validatePoint(center);
-        self.center = center;
-    };
-
-    self.addForce = function (force, forcePoint) {
-        var self = this;
-        self.forces.push(force);
-        self.forcePoints.push(forcePoint);
-    };
-
-    self.removeForce = function(index){
-        var self = this;
-        self.forces.splice(index,1);
-        self.forcePoints.splice(index,1);
-    };
-
-    self.getRotationMatrix = function () {
-        var self = this;
-        // only recompute if theta has changed since the last call.
-        var theta = self.shape.theta;
-        if (theta !== rotMatTheta || rotationMatrix == null) {
-            rotationMatrix = [
-                [Math.cos(theta), -Math.sin(theta)],
-                [Math.sin(theta), Math.cos(theta)]
-            ];
-            rotMatTheta = theta;
-        }
-        return rotationMatrix;
-    };
-
-    self.getAABB = function(){
-        var self = this;
-        if(self.AABB == null){
-            self.AABB = getAABB2(self.getVerticesInWorldCoords());
-        }
-        return self.AABB;
-    };
-
-    self.getVerticesInWorldCoords = function () {
-        var self = this;
-        if(self.vertsAbsolute == null){
-           self.vertsAbsolute = [];
-            var rotationMatrix = self.getRotationMatrix(), shape = self.shape,
-                vertices = shape.vertices, size = vertices.length, center = self.center, i;
-            for (i = 0; i < size; i++) {
-                self.vertsAbsolute.push(MV.VpV(center, MV.MxV(rotationMatrix, vertices[i])));
-            }
-        }
-
-        return self.vertsAbsolute;
-    };
-
+    self.rotMatTheta = null; // used to avoid unnecessary rotation matrix computations
+    self.rotationMatrix = null;
     self.update();
     self.getRotationMatrix();
+}
+
+Body.prototype.addLeaf = function(quad){
+    var self = this;
+    self.inLeaf.push(quad);
 };
+
+Body.prototype.update = function () {
+    var self = this;
+    self.mass = self.dinamic ? self.material.density * self.shape.area : 0;
+    self.mInv = self.mass == 0 ? 0 : 1 / self.mass; // inverse of the mass
+    self.moiInv = self.mass == 0 ? 0 : 1 / self.shape.moi(self.mass);// inverse of the moment of inertia
+    self.forces = [];
+    if (self.dinamic) {
+        self.forces.push([0, 98.81 * self.mass]);
+    }
+    else {
+        self.vLin = [0, 0];
+        self.vAng = 0;
+    }
+    self.rotationMatrix = null;
+};
+
+Body.prototype.setDinamic = function (dinamic) {
+    var self = this;
+    self.dinamic = dinamic;
+    self.update();
+};
+
+Body.prototype.setMass = function (mass) {
+    var self = this;
+    self.mass = mass;
+    self.mInv = self.mass == 0 ? 0 : 1 / self.mass; // inverse of the mass
+    self.moiInv = self.mass == 0 ? 0 : 1 / self.shape.moi(self.mass);// inverse of the moment of inertia
+    self.rotationMatrix = null;
+};
+
+Body.prototype.setShape = function (shape) {
+    var self = this;
+    self.shape = shape;
+    self.shape.parent = self;
+    self.update();
+};
+
+Body.prototype.setMaterial = function (material) {
+    var self = this;
+    self.material = material;
+    self.update();
+};
+
+Body.prototype.setCenter = function(center){
+    Point.validatePoint(center);
+    var self = this;
+    self.center = center;
+};
+
+Body.prototype.addForce = function (force, forcePoint) {
+    var self = this;
+    self.forces.push(force);
+    self.forcePoints.push(forcePoint);
+};
+
+Body.prototype.removeForce = function(index){
+    var self = this;
+    self.forces.splice(index,1);
+    self.forcePoints.splice(index,1);
+};
+
+Body.prototype.getRotationMatrix = function () {
+    var self = this;
+    // only recompute if theta has changed since the last call.
+    var theta = self.shape.theta;
+    if (theta !== self.rotMatTheta || self.rotationMatrix == null) {
+        self.rotationMatrix = [
+            [Math.cos(theta), -Math.sin(theta)],
+            [Math.sin(theta), Math.cos(theta)]
+        ];
+        self.rotMatTheta = theta;
+    }
+    return self.rotationMatrix;
+};
+
+Body.prototype.getAABB = function(){
+    var self = this;
+    if(self.AABB == null){
+        self.AABB = getAABB2(self.getVerticesInWorldCoords());
+    }
+    return self.AABB;
+};
+
+Body.prototype.getVerticesInWorldCoords = function () {
+    var self = this;
+    if(self.vertsAbsolute == null){
+        self.vertsAbsolute = [];
+        var rotationMatrix = self.getRotationMatrix(), shape = self.shape,
+            vertices = shape.vertices, size = vertices.length, center = self.center, i;
+        for (i = 0; i < size; i++) {
+            self.vertsAbsolute.push(MV.VpV(center, MV.MxV(rotationMatrix, vertices[i])));
+        }
+    }
+
+    return self.vertsAbsolute;
+};
+
 
 Body.validateBody = function(body){
     if(!(body instanceof Body)){
