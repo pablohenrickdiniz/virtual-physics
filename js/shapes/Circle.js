d6349f03488615de7a0219e943f34f453d6def95
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-01-19 07:03:19 -0300

    protótipo Circle

--- js/shapes/Circle.js
+++ js/shapes/Circle.js
@@ -3,19 +3,21 @@ Circle.prototype = new Arc([0,0],1,0,360);
 function Circle(center, radius) {
     var self = this;
     Arc.call(this, center, radius, 0, 360);
-    self.update = function () {
-        var self = this;
-        self.area = Math.PI * self.radius * self.radius;
-    };
+}
 
-    self.setRadius = function (radius) {
-        Arc.validateRadius(radius);
-        self.radius = radius;
-        self.update();
-    };
+Circle.prototype.update = function () {
+    var self = this;
+    self.area = Math.PI * self.radius * self.radius;
+};
 
-    self.moi = function (mass) {
-        var self = this;
-        return mass * self.radius * radius / 2;
-    };
+Circle.prototype.setRadius = function (radius) {
+    Arc.validateRadius(radius);
+    var self = this;
+    self.radius = radius;
+    self.update();
+};
+
+Circle.prototype.moi = function (mass) {
+    var self = this;
+    return mass * self.radius * radius / 2;
 };
