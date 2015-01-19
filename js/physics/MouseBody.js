fe7aba43d465795719e21dc73c0960a28e1ab156
Pablo Henrick Diniz <phydokz@hotmail.com>
2015-01-19 07:06:09 -0300

    protótipo MouseBody

--- js/physics/MouseBody.js
+++ js/physics/MouseBody.js
@@ -14,32 +14,33 @@ function MouseBody(shape) {
     self.shape = shape;
     self.center = shape.center;
 
-    var rotMatTheta; // used to avoid unnecessary rotation matrix computations
-    var rotationMatrix;
-
-    self.getVerticesInWorldCoords = function () {
-        var self = this;
-        var vertsAbsolute = [];
-        var rotationMatrix = self.getRotationMatrix();
-        var i;
-        var size =  self.shape.vertices.length;
-        for (i = 0; i < size;i++) {
-            vertsAbsolute.push(MV.VpV(self.center,
-                MV.MxV(rotationMatrix, self.shape.vertices[i])));
-        }
-        return vertsAbsolute;
-    };
-
-    self.getRotationMatrix = function () {
-        var self = this;
-        // only recompute if theta has changed since the last call.
-        if (self.shape.theta !== rotMatTheta || rotationMatrix == null) {
-            rotationMatrix = [
-                [Math.cos(self.shape.theta), -Math.sin(self.shape.theta)],
-                [Math.sin(self.shape.theta), Math.cos(self.shape.theta)]
-            ];
-            rotMatTheta = self.shape.theta;
-        }
-        return rotationMatrix;
-    }
+    self.rotMatTheta = null; // used to avoid unnecessary rotation matrix computations
+    self.rotationMatrix = null;
 }
+
+
+MouseBody.prototype.getVerticesInWorldCoords = function () {
+    var self = this;
+    var vertsAbsolute = [];
+    var rotationMatrix = self.getRotationMatrix();
+    var i;
+    var size =  self.shape.vertices.length;
+    for (i = 0; i < size;i++) {
+        vertsAbsolute.push(MV.VpV(self.center,
+            MV.MxV(rotationMatrix, self.shape.vertices[i])));
+    }
+    return vertsAbsolute;
+};
+
+MouseBody.prototype.getRotationMatrix = function () {
+    var self = this;
+    // only recompute if theta has changed since the last call.
+    if (self.shape.theta !== self.rotMatTheta || self.rotationMatrix == null) {
+        self.rotationMatrix = [
+            [Math.cos(self.shape.theta), -Math.sin(self.shape.theta)],
+            [Math.sin(self.shape.theta), Math.cos(self.shape.theta)]
+        ];
+        self.rotMatTheta = self.shape.theta;
+    }
+    return self.rotationMatrix;
+};
